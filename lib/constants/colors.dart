d2c3ecb653d27d8ffc8c37a19db4561c0d927aa9
Mozammel Hoshen Chowdhury <mozammel990@gmail.com>
2022-09-27 22:54:13 +0600

    working on product search page

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/cupertino.dart';
 
 const backgroundColor = Color(0xFFF7F2FF);
+const whiteBack = Color(0xFFFFFFFF);
+const naturalBlack = Color(0xFF323232);
+const naturalBlack300 = Color(0xFF646464);
+const secondaryColor = Color(0xFFDA2079);
--- lib/screens/product.dart
+++ lib/screens/product.dart
@@ -1,5 +1,6 @@
 import 'package:e_commerce/constants/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class ProductPage extends StatelessWidget {
   const ProductPage({Key? key}) : super(key: key);
@@ -8,6 +9,179 @@ class ProductPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
+      appBar: AppBar(
+        backgroundColor: backgroundColor,
+        elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 48,
+                width: MediaQuery.of(context).size.width * 0.9,
+                decoration: BoxDecoration(
+                  color: whiteBack,
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                child: TextFormField(
+                  decoration: const InputDecoration(
+                    isDense: true,
+                    enabledBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.transparent, width: 1.0),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide:
+                          BorderSide(color: Colors.transparent, width: 1.0),
+                    ),
+                    suffixIcon: Icon(Icons.search_outlined),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 15,
+          ),
+          Expanded(
+            child: SingleChildScrollView(
+              child: GridView.builder(
+                padding: const EdgeInsets.fromLTRB(15, 5, 15, 5),
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  childAspectRatio: 1,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 5,
+                  crossAxisSpacing: 10,
+                  mainAxisExtent: 267,
+                ),
+                itemCount: 6,
+                itemBuilder: (context, index) {
+                  return InkWell(
+                    onTap: () {},
+                    child: SizedBox(
+                      child: Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        elevation: 0,
+                        color: whiteBack,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image.asset("assets/images/potato_chips.png"),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            const Text(
+                              "Lays Classic Family Chips",
+                              style: TextStyle(
+                                  color: naturalBlack,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w500),
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "ক্রয়",
+                                      style: TextStyle(
+                                          color: naturalBlack300,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "৳ 20.00",
+                                      style: TextStyle(
+                                          color: secondaryColor,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "৳ 22.00",
+                                      style: TextStyle(
+                                          color: secondaryColor,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "বিক্রয়",
+                                      style: TextStyle(
+                                          color: naturalBlack300,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "৳ 20.00",
+                                      style: TextStyle(
+                                          color: naturalBlack300,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                                Row(
+                                  children: const [
+                                    Text(
+                                      "লাভ",
+                                      style: TextStyle(
+                                          color: naturalBlack300,
+                                          fontSize: 12,
+                                          fontWeight: FontWeight.w400),
+                                    ),
+                                    SizedBox(
+                                      width: 5,
+                                    ),
+                                    Text(
+                                      "৳ 5.00",
+                                      style: TextStyle(
+                                          color: naturalBlack300,
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w700),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
